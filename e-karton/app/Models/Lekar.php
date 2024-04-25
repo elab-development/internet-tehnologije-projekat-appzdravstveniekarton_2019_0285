5a8e03fd5213413888b698b01d969a7673781f9d
Sofija Vasic <vasicsofija7@gmail.com>
2024-04-26 01:18:14 +0200

    Kreiranje modela i dodavanje atributa koje ce imati model

---
+++ e-karton/app/Models/Lekar.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Lekar extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'ime', 'prezime', 'specijalizacija'
+    ];
+}
