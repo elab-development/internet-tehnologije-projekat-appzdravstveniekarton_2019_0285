af649c4001d3519c29012b5d45d556db9f430f07
Stefan Vasiljevic <stefan.vasiljevic00@gmail.com>
2024-09-10 20:24:34 +0200

    Kreiranje ruta

--- React/zdravstveni-e-karton/src/App.js
+++ React/zdravstveni-e-karton/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/NavBar';
+import Login from './pages/Login';
+import Dijagnoze from './pages/Dijagnoze';
+import KontaktDoktori from './pages/KontaktDoktori';
+import Register from './pages/Register';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+      <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/dijagnoze" element={<Dijagnoze />} />
+        <Route path="/kontakt" element={<KontaktDoktori />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
